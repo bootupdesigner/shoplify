882fc685acc7717ce21c4997d8e7150bff2dd03f
bootupdesigner <bootup.itinfo@gmail.com>
2021-11-29 17:09:20 -0500

    removed payment type card from checkout controller

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,7 +3,7 @@ class CheckoutController < ApplicationController
 def create
     product = Product.find(params[:id])
    @session = Stripe::Checkout::Session.create({
-payment_method_types: ['card'],
+
 
   line_items: [{ 
       name: product.name,
